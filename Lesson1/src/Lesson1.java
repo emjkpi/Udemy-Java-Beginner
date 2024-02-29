0f0a41928aacc601d4f7c4912718d8cb4f1bc7f2
emzar.kvitsyniia <emzar.kvitsyniia@pragmaticplay.com>
2024-02-29 18:57:07 +0100

    1 lesson

---
+++ Lesson1/src/Lesson1.java
@@ -0,0 +1,11 @@
+public class Lesson1 {
+    public static void main(String[] args) {
+
+        System.out.println("           Sigh No More");
+        System.out.println("Sigh no more, ladies, sigh no more,");
+        System.out.println("Men were deceivers ever,");
+        System.out.println("One foot in sea and one on shore,");
+        System.out.println("To one thing constant never.");
+        System.out.println("                     William Shakespeare");
+    }
+}
